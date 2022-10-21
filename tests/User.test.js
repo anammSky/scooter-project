602b1c0ddadb83496bcd1f0674f42dc815ec153f
Ana Moros <ana.morosmarmol@sky.uk>
2022-10-21 10:39:14 +0100

    added tests for User porperties

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,9 +1,20 @@
-const User = require('../src/User')
+const User = require("../src/User");
 
 // User tests here
-
-// test username
-
-// test password
-
-// test age
+describe("Checks the properties of user", () => {
+  beforeEach(() => {
+    newUser = new User("John", "123456789", 23);
+  });
+  // test username
+  test("should set the username", () => {
+    expect(newUser.username).toMatch("John");
+  });
+  // test password
+  test("should set the username", () => {
+    expect(newUser.password).toMatch("123456789");
+  });
+  // test age
+  test("should set the username", () => {
+    expect(newUser.age).toBe(23);
+  });
+});
